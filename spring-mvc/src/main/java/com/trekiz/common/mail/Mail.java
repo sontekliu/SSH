c2161679d50d1ddc815ec8c060bee6f26d6c9ef5
javaliu <sontekliu@outlook.com>
2016-07-28 10:39:26 +0800

    delete some config file

--- spring-mvc/src/main/java/com/trekiz/common/mail/Mail.java
+++ spring-mvc/src/main/java/com/trekiz/common/mail/Mail.java
@@ -2,12 +2,12 @@ package com.trekiz.common.mail;
 
 public interface Mail {
 
-	public static final String SINA_PASSWD = "lsj_sina_1521";
+	public static final String SINA_PASSWD = "123456";
 	public static final String SINA_USER = "sontek@sina.cn";
 	
-	public static final String PASSWD_163 = "lsj_163_1521";
+	public static final String PASSWD_163 = "123456";
 	public static final String USER_163 = "jianhualaoliu@163.com";
 	
-	public static final String PASSWD_QUAUQ = "lsj_jhll_1521";
+	public static final String PASSWD_QUAUQ = "123456";
 	public static final String USER_QUAUQ = "shijun.liu@quauq.com";
 }
